d1d06e372442049db6d3060fbc342d780ec81d4d
Mahadevii <mshadrina@gmail.com>
2023-06-26 06:26:55 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp7/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Panel1 = New Panel()
+        CheckBox2 = New CheckBox()
+        Button2 = New Button()
+        Label5 = New Label()
+        CheckBox1 = New CheckBox()
+        ComboBox4 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        ComboBox1 = New ComboBox()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ButtonHighlight
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.Font = New Font("Bookman Old Style", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Button1.ForeColor = SystemColors.ControlText
+        Button1.Location = New Point(12, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(131, 64)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.ButtonHighlight
+        Panel1.BorderStyle = BorderStyle.FixedSingle
+        Panel1.Controls.Add(CheckBox2)
+        Panel1.Controls.Add(Button2)
+        Panel1.Controls.Add(Label5)
+        Panel1.Controls.Add(CheckBox1)
+        Panel1.Controls.Add(ComboBox4)
+        Panel1.Controls.Add(ComboBox3)
+        Panel1.Controls.Add(ComboBox2)
+        Panel1.Controls.Add(ComboBox1)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(139, 224)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(532, 366)
+        Panel1.TabIndex = 1
+        Panel1.Visible = False
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(485, 3)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(103, 24)
+        CheckBox2.TabIndex = 11
+        CheckBox2.Text = "CheckBox2"
+        CheckBox2.TextImageRelation = TextImageRelation.TextBeforeImage
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(416, 312)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 10
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(26, 321)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(53, 20)
+        Label5.TabIndex = 9
+        Label5.Text = "Label5"' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(183, 173)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(103, 24)
+        CheckBox1.TabIndex = 8
+        CheckBox1.Text = "CheckBox1"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Location = New Point(26, 266)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(151, 28)
+        ComboBox4.TabIndex = 7
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Location = New Point(26, 171)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(151, 28)
+        ComboBox3.TabIndex = 6
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(303, 82)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(151, 28)
+        ComboBox2.TabIndex = 5
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(26, 82)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(26, 222)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(53, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Label4"' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(26, 132)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Label3"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(303, 49)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Label2"' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(26, 49)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(1053, 698)
+        Controls.Add(Panel1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Travel shop Airlines"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
---
+++ WinFormsApp7/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim ri As Integer
+    Dim rj As Integer
+    Dim kilom = {{0, 7093, 690, 3834, 2085, 1727, 2728},
+                 {7093, 0, 6645, 4247, 5055, 6516, 4507},
+                 {690, 6645, 0, 3794, 1828, 2110, 2502},
+                 {3834, 4247, 3794, 0, 2057, 2537, 1467},
+                 {2085, 5055, 1828, 2057, 0, 1784, 674},
+                 {1727, 6516, 2110, 2537, 1784, 0, 2083},
+                 {2728, 4507, 2502, 1467, 674, 2083, 0}}
+    Dim speedAirplane = 700
+    Dim res = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Место отправления"
+        Label2.Text = "Место прибытия"
+        Label3.Text = "Условия обслуживания"
+        Label4.Text = "Расположение места"
+        Label5.Text = ""
+        Button1.Text = "Выбрать билет"
+        Button2.Text = "Рассчитать"
+        CheckBox1.Text = "Салон для курения"
+        CheckBox2.Text = "X"
+        ComboBox1.Items.AddRange({"Анапа", "Владивосток", "Грозный", "Диксон",
+                                  "Екатеринбург", "Санкт-Петербург", "Ханты-Мансийск"})
+        ComboBox2.Items.AddRange({"Анапа", "Владивосток", "Грозный", "Диксон",
+                                  "Екатеринбург", "Санкт-Петербург", "Ханты-Мансийск"})
+        ComboBox3.Items.AddRange({"Эконом", "Премиум", "Бизнес"})
+        ComboBox4.Items.AddRange({"Окно", "Середина", "У выхода"})
+    End Sub
+    Private Sub Case1(sender As Object, e As EventArgs) Handles Button2.Click
+        res = 0
+        If ComboBox1.SelectedIndex < 0 Then
+            MsgBox("Выберите город отправления")
+        Else
+            Dim F1 = ComboBox1.SelectedIndex
+            Select Case F1
+                Case 0
+                    ri = 0
+                Case 1
+                    ri = 1
+                Case 2
+                    ri = 2
+                Case 3
+                    ri = 3
+                Case 4
+                    ri = 4
+                Case 5
+                    ri = 5
+                Case 6
+                    ri = 6
+            End Select
+            Case2(F1)
+        End If
+    End Sub
+    Private Sub Case2(F1 As Integer)
+        Dim F2 = ComboBox2.SelectedIndex
+        If F1 = F2 Then
+            MsgBox("Выберите разные города")
+        Else
+            If ComboBox2.SelectedIndex < 0 Then
+                MsgBox("Выберите город прибытия!")
+            Else
+                Select Case F2
+                    Case 0
+                        rj = 0
+                    Case 1
+                        rj = 1
+                    Case 2
+                        rj = 2
+                    Case 3
+                        rj = 3
+                    Case 4
+                        rj = 4
+                    Case 5
+                        rj = 5
+                    Case 6
+                        rj = 6
+                End Select
+                res = kilom(ri, rj) / speedAirplane * 1420
+                Case3()
+            End If
+        End If
+    End Sub
+    Private Sub Case3()
+        Dim F3 = ComboBox3.SelectedIndex
+        If ComboBox3.SelectedIndex < 0 Then
+            MsgBox("Выберите условия обслуживания!")
+        Else
+            Select Case F3
+                Case 0
+                    res = res * 1.1
+                Case 1
+                    res = res * 1.25
+                Case 2
+                    res = res * 1.5
+            End Select
+            Case5()
+        End If
+    End Sub
+    Private Sub Case5()
+        If CheckBox1.Checked Then
+            res += 3000
+        End If
+        Case4()
+    End Sub
+    Private Sub Case4()
+        Dim F4 = ComboBox4.SelectedIndex
+        If ComboBox4.SelectedIndex < 0 Then
+            MsgBox("Выберите условия обслуживания!")
+        Else
+            Select Case F4
+                Case 0
+                    res = res * 1.5
+                Case 1
+                    res = res * 1.25
+                Case 2
+                    res = res * 1.1
+            End Select
+            Out_Label()
+        End If
+    End Sub
+    Private Sub Out_Label()
+        Label5.Text = "Стоимость билета: " + Str(Math.Round(res)) + "р."
+    End Sub
+    Private Sub ShowPanel(sender As Object, e As EventArgs) Handles Button1.Click
+        Panel1.Visible = True
+    End Sub
+    Private Sub HidePanel(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        Panel1.Visible = False
+        CheckBox2.CheckState = False
+        CheckBox1.Checked = False
+        ComboBox1.SelectedIndex = -1
+        ComboBox2.SelectedIndex = -1
+        ComboBox3.SelectedIndex = -1
+        ComboBox4.SelectedIndex = -1
+    End Sub
+End Class
